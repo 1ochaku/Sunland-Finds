c26c1eca278884cf443c21fe6fec049caf3fe43a
Punyo Sunya <128706184+1ochaku@users.noreply.github.com>
2024-06-12 22:17:18 +0530

    Update firebaseConfig.jsx

--- firebaseConfig.jsx
+++ firebaseConfig.jsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 
 // My web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCGdg4QgFRTzujP4elbe5pkXANkuiyOOjg",
-  authDomain: "sunland-finds.firebaseapp.com",
-  projectId: "sunland-finds",
-  storageBucket: "sunland-finds.appspot.com",
-  messagingSenderId: "520525709211",
-  appId: "1:520525709211:web:83d1c91dc96f4178d56b3e",
-  measurementId: "G-GEMB2NZWTC"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
